8d4fd0eb5b7998c4f9a7ede582f6f85d1aecd3f3
Khurram Shehzad <khurram.pak522@gmail.com>
2020-03-30 16:30:31 +0500

    Initial Commit

---
+++ Parallex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parallex
+//
+//  Created by Khurram on 30/03/2020.
+//  Copyright © 2020 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
